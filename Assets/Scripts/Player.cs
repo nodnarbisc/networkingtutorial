dc763999907abba3779eaba44006a5fb99f52356
nodnarbisc <brandon-campbell@pluralsight.com>
2022-03-17 14:35:04 -0600

    added commands, and targetrpc

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
+
+    [SyncVar(hook = "OnHolaCountChange")]
+    int holaCount = 0;
     void HandleMovement()
     {
         if (isLocalPlayer)
@@ -22,7 +25,47 @@ public class Player : NetworkBehaviour
     private void Update()
     {
         HandleMovement();
+
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Sending Hola to Server.");
+            Hola();
+        }
+
+        if (isServer && transform.position.y > 5)
+        {
+            TooHigh();
+        }
     }
+
+    [ClientRpc]
+    void TooHigh()
+    {
+        Debug.Log("Too High!");
+    }
+
+
+    [Command]
+    void Hola()
+    {
+        Debug.Log("Recieved Hola from Client");
+        ReplyHola();
+        holaCount++;
+    }
+
+    [TargetRpc]
+    void ReplyHola()
+    {
+        Debug.Log("Recieved Hola from Server");
+    }
+
+    void OnHolaCountChange(int oldCount, int newCount)
+    {
+
+        Debug.Log($"We had {oldCount} holas, but now we have {newCount} holas");
+
+    }
+
 }
 
 
